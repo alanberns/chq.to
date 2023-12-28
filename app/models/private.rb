2386979ee43e5741997a0e59b358aef7d3c5e262
Alan <alan_berns@yahoo.com.ar>
2023-12-28 11:41:37 -0300

    cambiado password de link privado

--- app/models/private.rb
+++ app/models/private.rb
@@ -1,5 +1,5 @@
 class Private < Link
-    validates :password, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true
-    validates :password, length: { in: 4..25, message: "lenght is 4-25" }
+    validates :password, format: { with: /\A[a-zA-Z0-9-.,!#¡¿?*+]+\z/, message: "only allows letters" }, presence: true
+    validates :password, length: { in: 8..25, message: "lenght is 8-25" }
     has_secure_password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     
     validates :username, :email, :password, presence: true
     validates :username, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-    validates :username, :password, length: { in: 4..25, message: "lenght is 4-25" }
+    validates :username, :password, length: { in: 5..25, message: "lenght is 5-25" }
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email"}
     validates :username, :email, uniqueness: { message: "Already in use" }
     has_secure_password
